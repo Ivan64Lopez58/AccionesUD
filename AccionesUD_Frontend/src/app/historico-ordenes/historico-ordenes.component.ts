22cdf367271f2989b9a170d95eca4a5d045ceaed
Julián Espinosa <juliancamilosimpson@gmail.com>
2025-06-19 18:03:36 -0500

    Creación de componente de historico de ordenes

---
+++ AccionesUD_Frontend/src/app/historico-ordenes/historico-ordenes.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-historico-ordenes',
+  templateUrl: './historico-ordenes.component.html',
+  styleUrls: ['./historico-ordenes.component.css']
+})
+export class HistoricoOrdenesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  
+
+}
