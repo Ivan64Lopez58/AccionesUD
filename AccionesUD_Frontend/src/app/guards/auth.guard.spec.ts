f2da90235fb3c8c0fab1cf73d3478fc3a8ced585
SalazarMalagon <andressalazar062858@gmail.com>
2025-06-23 14:31:07 -0500

    Correccion error authguard

--- AccionesUD_Frontend/src/app/guards/auth.guard.spec.ts
+++ AccionesUD_Frontend/src/app/guards/auth.guard.spec.ts
@@ -24,14 +24,14 @@ describe('AuthGuard', () => {
     localStorage.clear();
   });
 
-  it('debería redirigir si no hay token', () => {
+  it('should redirect if there is no token', () => {
     localStorage.removeItem('jwt');
     const result = guard.canActivate();
     expect(routerSpy.parseUrl).toHaveBeenCalledWith('/');
     expect(result instanceof UrlTree).toBeTrue();
   });
 
-  it('debería redirigir si el token está expirado', () => {
+  it('should redirect if the token is expired', () => {
     // Token expirado: fecha de expiración pasada
     const expiredToken = generarTokenConExpiracion(-60); // hace 1 minuto
     localStorage.setItem('jwt', expiredToken);
@@ -42,7 +42,7 @@ describe('AuthGuard', () => {
     expect(result instanceof UrlTree).toBeTrue();
   });
 
-  it('debería permitir el acceso si el token es válido', () => {
+  it('should allow access if the token is valid', () => {
     const validToken = generarTokenConExpiracion(60); // 1 minuto en el futuro
     localStorage.setItem('jwt', validToken);
 
