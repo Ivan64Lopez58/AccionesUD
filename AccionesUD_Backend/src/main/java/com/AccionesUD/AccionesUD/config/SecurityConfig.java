2c0726958f18a90733e21502b5bad56e15b61e8a
Iván López GitHub <idlopezd@udistrital.edu.co>
2025-07-03 02:41:52 -0500

    Fix

--- AccionesUD_Backend/src/main/java/com/AccionesUD/AccionesUD/config/SecurityConfig.java
+++ AccionesUD_Backend/src/main/java/com/AccionesUD/AccionesUD/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,25 +24,23 @@ public class SecurityConfig {
     private final AuthenticationProvider authProvider;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
-    {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-            .cors(cors -> {})
+            .cors(Customizer.withDefaults())  // Usar la nueva sintaxis recomendada para CORS
             .csrf(csrf -> 
-                csrf
-                .disable())
+                csrf.disable())  // Desactivar CSRF si no lo necesitas
             .authorizeHttpRequests(authRequest ->
-              authRequest
-                .requestMatchers("/auth/**").permitAll()
-                .requestMatchers("/api/**").permitAll()
-                .requestMatchers("/acciones/scrap").permitAll()
-                .anyRequest().authenticated()
-                )
-            .sessionManagement(sessionManager->
-                sessionManager 
-                  .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                authRequest
+                    .requestMatchers("/auth/**").permitAll()  // Rutas públicas para autenticación
+                    .requestMatchers("/api/**").permitAll()   // Rutas públicas para la API
+                    .requestMatchers("/acciones/scrap").permitAll()  // Rutas públicas para scrapping
+                    .anyRequest().authenticated()  // Requiere autenticación para el resto de las rutas
+            )
+            .sessionManagement(sessionManager -> 
+                sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // Usar sesión sin estado
+            )
             .authenticationProvider(authProvider)
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)  // Añadir filtro JWT
             .build();
-        }
     }
+}
