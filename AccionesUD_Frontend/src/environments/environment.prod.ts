09e83cbec99f90415685b05d2698b8cad98308f0
Iván López GitHub <idlopezd@udistrital.edu.co>
2025-07-03 00:14:35 -0500

    Corrección

---
+++ AccionesUD_Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://accionesud-backend.up.railway.app',
+  enableDebug: false,
+  version: '1.0.0',
+  defaultLanguage: 'es'
+};
